2b080000cf91e52cb8bafd475c24aca0293cfba3
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-05-31 14:19:13 +0900

    Initial Commit

---
+++ swift-to-do/swift_to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_to_doApp.swift
+//  swift-to-do
+//
+//  Created by SUNGIL-POS on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct swift_to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
